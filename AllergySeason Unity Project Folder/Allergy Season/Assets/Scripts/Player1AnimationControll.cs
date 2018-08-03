daea3af276ac20b6cf35d10c3ba82dc737b82588
BugFrienD <42068315+BugFrienD@users.noreply.github.com>
2018-08-04 00:55:09 +0800

    Player1Animation

---
+++ AllergySeason Unity Project Folder/Allergy Season/Assets/Scripts/Player1AnimationControll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1AnimationControll : MonoBehaviour {
+
+    Animator Player1Animator;
+    public KeyCode LeftMove;
+    public KeyCode RightMove;
+
+	// Use this for initialization
+	void Start () {
+        Player1Animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(LeftMove) || Input.GetKey(RightMove))
+        {
+            Player1Animator.SetBool("Player1Move", true);
+        }
+        else
+        {
+            Player1Animator.SetBool("Player1Move", false);
+        }
+	}
+}
